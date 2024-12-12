6ccd87d65773e7eeea0b8fc36af098615af7b052
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-12-12 13:53:56 +0000

    Set up basic views and url route.

--- project_root/urls.py
+++ project_root/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from christmas import views as christmas_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', christmas_views.index, name='index'),
+   
 ]
