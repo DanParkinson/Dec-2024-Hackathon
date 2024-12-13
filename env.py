3b85bdefe1b1944090a93f6440d64622c32c7162
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-12-13 10:35:06 +0000

    Link up Postgress database, add to requirements.txt

--- env.py
+++ env.py
@@ -1,3 +1,6 @@
 import os
 
 os.environ.setdefault("SECRET_KEY", "85l=ut+-(6v13f2ppq&d4bs-jdt!xm4&_bq05jlck=4j&8^x%r")
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:EYTC6Qk5piMI@ep-lucky-moon-a275id6k.eu-central-1.aws.neon.tech/spoon_knee_tidy_897992")
