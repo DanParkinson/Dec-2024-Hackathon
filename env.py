3b4927baa04a7bcb9daf3795155d88bbfab136bb
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-12-14 11:52:32 +0000

    Delete env.py.

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "85l=ut+-(6v13f2ppq&d4bs-jdt!xm4&_bq05jlck=4j&8^x%r")
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:EYTC6Qk5piMI@ep-lucky-moon-a275id6k.eu-central-1.aws.neon.tech/spoon_knee_tidy_897992")
-
-os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://867948474894481:yUDvbWc1jHlx55z5nauf6wR7kDE@doauajauo")
